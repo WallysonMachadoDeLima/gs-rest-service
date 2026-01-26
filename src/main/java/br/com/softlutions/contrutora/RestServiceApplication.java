aefa06e1c936b0ff4a68aa4f2d79a8d7d3e6e6d3
wallyson machado <machadodelimawallyson@gmail.com>
2026-01-25 21:11:19 -0400

    Add Makefile for Docker setup; enhance TerrenoController with @NonNull annotations for improved null safety

--- src/main/java/br/com/softlutions/contrutora/RestServiceApplication.java
+++ src/main/java/br/com/softlutions/contrutora/RestServiceApplication.java
@@ -2,8 +2,14 @@ package br.com.softlutions.contrutora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"br.com.softlutions.contrutora", "br.com.softlutions.contrutora.modules", "br.com.softlutions.shared"})
+@EntityScan(basePackages = {"br.com.softlutions.contrutora.modules", "br.com.softlutions.shared"})
+@EnableJpaRepositories(basePackages = {"br.com.softlutions.contrutora.modules"})
 public class RestServiceApplication {
 
   public static void main(String[] args) {
--- src/main/java/br/com/softlutions/contrutora/modules/terreno/controllers/TerrenoController.java
+++ src/main/java/br/com/softlutions/contrutora/modules/terreno/controllers/TerrenoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,26 +31,26 @@ public class TerrenoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Terreno> buscar(@PathVariable Integer id) {
+    public ResponseEntity<Terreno> buscar(@PathVariable @NonNull Integer id) {
         return service.findOne(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Terreno criar(@RequestBody Terreno terreno) {
+    public Terreno criar(@RequestBody @NonNull Terreno terreno) {
         return service.create(terreno);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Terreno> atualizar(@PathVariable Integer id, @RequestBody Terreno dados) {
+    public ResponseEntity<Terreno> atualizar(@PathVariable @NonNull Integer id, @RequestBody @NonNull Terreno dados) {
         return service.update(id, dados)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+    public ResponseEntity<Void> deletar(@PathVariable @NonNull Integer id) {
         if (service.delete(id)) {
             return ResponseEntity.noContent().build();
         }
